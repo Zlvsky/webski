d5b28a1cb4b0b1b49fedd19d2487e031d13632d5
Chris.Z <zlvsky@icloud.com>
2019-02-23 23:03:23 +0100

    initial Commit

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ function scrollFunction() {
     document.getElementById("navbar").style.backgroundColor = "#191919";
     document.getElementById("navbar").style.color = "white";
     document.getElementById("logo").style.color = "white";
-    document.getElementById("hyperlink").style.color = "white";
+    document.getElementById("nothingA").style.color = "white";
+    document.querySelector(".hyperlink").style.color = "white";
     document.getElementById("navbar").style.fontSize = "19px";
     document.getElementById("navbar").style.height = "9%";
     document.getElementById("nav-wrap").style.marginTop = "8px";
@@ -15,7 +16,8 @@ function scrollFunction() {
     document.getElementById("navbar").style.backgroundColor = "transparent";
     document.getElementById("navbar").style.color = "black";
     document.getElementById("logo").style.color = "black";
-    document.getElementById("hyperlink").style.color = "black";
+    document.getElementById("nothingA").style.color = "black";
+    document.querySelector(".hyperlink").style.color = "black";
     document.getElementById("navbar").style.fontSize = "19px";
     document.getElementById("navbar").style.height = "12%";
     document.getElementById("nav-wrap").style.marginTop = "30px";
